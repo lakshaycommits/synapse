d09a0b4be9a92d7c96baecf137ff97516a392707
Lakshay Chauhan <imlakshaychauhan@gmail.com>
2026-06-07 23:16:37 +0530

    hugging face api

--- app/utils/embeddings.py
+++ app/utils/embeddings.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from dotenv import load_dotenv
 import os
 
@@ -6,7 +6,10 @@ load_dotenv()
 
 class Embeddings:
     def __init__(self):
-        self._instance = HuggingFaceEmbeddings(model_name=os.getenv("EMBEDDING_MODEL"))
+        self._instance = HuggingFaceInferenceAPIEmbeddings(
+            api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+            model_name=os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2"),
+        )
 
-    def instance(self) -> HuggingFaceEmbeddings:
+    def instance(self) -> HuggingFaceInferenceAPIEmbeddings:
         return self._instance
